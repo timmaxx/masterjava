8a6fbe202fe970515d269b1e4ab64a6f75d1b362
“Maksim <m.n.timchenko@gmail.com>
2023-01-29 18:08:35 +1000

    1. Optimize MatrixUtil.singleThreadMultiply

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,38 +13,150 @@ public class MainMatrix {
     private static final int MATRIX_SIZE = 1000;
     private static final int THREAD_NUMBER = 10;
 
+    private static final int COUNT_OF_PASS = 5;
+
     private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
-        final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
+        System.out.println("Enter size of matrix. Enter 1000");
+        Scanner scanner = new Scanner(System.in);
+        // Специально сделано не через константу. Для проверки на шаге 0.
+        int matrix_size = scanner.nextInt();
 
+        // final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
+        // final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
+
+        final int[][] matrixA = MatrixUtil.create(matrix_size);
+        final int[][] matrixB = MatrixUtil.create(matrix_size);
+
+        double singleThreadSum0 = 0.;
         double singleThreadSum = 0.;
-        double concurrentThreadSum = 0.;
+        double singleThreadSum2 = 0.;
+        double singleThreadSum3 = 0.;
+        double singleThreadSum42 = 0.;
+        double singleThreadSum43 = 0.;
+        //double singleThreadSum543 = 0.;
+        //double concurrentThreadSum = 0.;
         int count = 1;
-        while (count < 6) {
+
+        while (count <= COUNT_OF_PASS) {
             System.out.println("Pass " + count);
-            long start = System.currentTimeMillis();
+            long start;
+            double duration;
+
+            start = System.currentTimeMillis();
+            final int[][] matrixC0 = MatrixUtil.singleThreadMultiply0(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (0), sec: %.3f", duration);
+            singleThreadSum0 += duration;
+
+            start = System.currentTimeMillis();
             final int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
-            double duration = (System.currentTimeMillis() - start) / 1000.;
+            duration = (System.currentTimeMillis() - start) / 1000.;
             out("Single thread time, sec: %.3f", duration);
             singleThreadSum += duration;
 
+            start = System.currentTimeMillis();
+            final int[][] matrixC2 = MatrixUtil.singleThreadMultiply2(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (2), sec: %.3f", duration);
+            singleThreadSum2 += duration;
+
+            start = System.currentTimeMillis();
+            final int[][] matrixC3 = MatrixUtil.singleThreadMultiply3(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (3), sec: %.3f", duration);
+            singleThreadSum3 += duration;
+
+            start = System.currentTimeMillis();
+            final int[][] matrixC42 = MatrixUtil.singleThreadMultiply42(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (42), sec: %.3f", duration);
+            singleThreadSum42 += duration;
+
+            start = System.currentTimeMillis();
+            final int[][] matrixC43 = MatrixUtil.singleThreadMultiply43(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (43), sec: %.3f", duration);
+            singleThreadSum43 += duration;
+/*
+            start = System.currentTimeMillis();
+            final int[][] matrixC543 = MatrixUtil.singleThreadMultiply543(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time (543), sec: %.3f", duration);
+            singleThreadSum543 += duration;
+*/
+            /*
             start = System.currentTimeMillis();
             final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
             duration = (System.currentTimeMillis() - start) / 1000.;
             out("Concurrent thread time, sec: %.3f", duration);
             concurrentThreadSum += duration;
-
-            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+*/
+            if (!MatrixUtil.compare(matrixC, matrixC2)) {
+                System.err.println("Comparison failed (2)");
+                break;
+            } else if (!MatrixUtil.compare(matrixC, matrixC0)) {
+                System.err.println("Comparison failed (0)");
+                break;
+            } else if (!MatrixUtil.compare(matrixC, matrixC3)) {
+                System.err.println("Comparison failed (3)");
+                break;
+            } else if (!MatrixUtil.compare(matrixC, matrixC42)) {
+                System.err.println("Comparison failed (42)");
+                break;
+            } else if (!MatrixUtil.compare(matrixC, matrixC43)) {
+                System.err.println("Comparison failed (43)");
+                break;
+            } /* else if (!MatrixUtil.compare(matrixC, matrixC543)) {
+                System.err.println("Comparison failed (543)");
+            }*/
+            /* else if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
                 System.err.println("Comparison failed");
                 break;
             }
+            */
             count++;
         }
         executor.shutdown();
-        out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
-        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+        System.out.println("\n");
+        out("Average single thread time (0), sec: %.3f", singleThreadSum0 / count);
+        out("Average single thread time, sec: %.3f", singleThreadSum / count);
+        out("Average single thread time (2), sec: %.3f", singleThreadSum2 / count);
+        out("Average single thread time (3), sec: %.3f", singleThreadSum3 / count);
+        out("Average single thread time (42), sec: %.3f", singleThreadSum42 / count);
+        out("Average single thread time (43), sec: %.3f", singleThreadSum43 / count);
+        //out("Average single thread time 543, sec: %.3f", singleThreadSum543 / count);
+        //out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / count);
+
+        /*
+        Average single thread time (0), sec: 4.710
+        Average single thread time, sec: 4.608
+        Average single thread time (2), sec: 0.512
+        Average single thread time (3), sec: 0.844
+        Average single thread time (42), sec: 1.636
+        Average single thread time (43), sec: 0.518
+        */
+
+        /*
+        Average single thread time (0), sec: 4.828
+        Average single thread time, sec: 4.784
+        Average single thread time (2), sec: 0.498
+        Average single thread time (3), sec: 0.849
+        Average single thread time (42), sec: 1.635
+        Average single thread time (43), sec: 0.507
+        */
+
+        /*
+        1. Шаг 0 немного менее производительный чем шаг 1. Ок.
+        2. Шаг 2 производительнее чем шаг 1 примерно в 10 раз. Ok.
+        3. Шаг 3 НЕ производительнее шага 2. ПОЧЕМУ? (не соответствует статье).
+        4. Шаг 42 НЕ производительнее шага 2. ПОЧЕМУ? (42 - моя собственная инициатива).
+        5. Шаг 43 производительнее шага 3. Ок.
+        6. Шаг 43 НЕ производительнее шага 2. ПОЧЕМУ? (не соответствует статье).
+
+        Из этих замеров, похоже, что самый быстрый, это шаг 2.
+        */
     }
 
     private static void out(String format, double ms) {
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -18,6 +18,23 @@ public class MatrixUtil {
         return matrixC;
     }
 
+    // Реализован шаг 0 из статьи.
+    // Предположение о деоптимизации в случае, когда отказываемся от границ матриц final на динамические.
+    public static int[][] singleThreadMultiply0(int[][] matrixA, int[][] matrixB) {
+        final int[][] matrixC = new int[matrixA.length][matrixA.length];
+
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA.length; j++) {
+                int sum = 0;
+                for (int k = 0; k < matrixA.length; k++) {
+                    sum += matrixA[i][k] * matrixB[k][j];
+                }
+                matrixC[i][j] = sum;
+            }
+        }
+        return matrixC;
+    }
+
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
@@ -31,6 +48,160 @@ public class MatrixUtil {
                 }
                 matrixC[i][j] = sum;
             }
+        }
+        return matrixC;
+    }
+
+    // Реализован Шаг 2 из статьи.
+    // Матрицу B транспонируем (BT) и тогда будет последовательный доступ к элементам BT.
+    // Получим максимальную выгоду от кеша.
+    public static int[][] singleThreadMultiply2(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        final int matrixBT[][] = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrixBT[j][i] = matrixB[i][j];
+            }
+        }
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                int sum = 0;
+                for (int k = 0; k < matrixSize; k++) {
+                    sum += matrixA[i][k] * matrixBT[j][k];
+                }
+                matrixC[i][j] = sum;
+            }
+        }
+        return matrixC;
+    }
+
+    // Реализован Шаг 4 из статьи на методе singleThreadMultiply2.
+    // Воспользуемся еще одним преимуществом Java — исключениями.
+    // Заменяем проверку на выход за границы матрицы (для внешнего цикла) на блок try {} catch {}.
+    // Это сократит количество сравнений на 1000 в нашем случае.
+    // Действительно, зачем 1000 раз сравнивать то, что всегда будет возвращать false и на 1001 раз вернет true.
+    //
+    // С одной стороны — сокращаем количество сравнений,
+    // с другой — появляется дополнительные накладные расходы на обработку исключений.
+    public static int[][] singleThreadMultiply42(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        final int matrixBT[][] = new int[matrixSize][matrixSize];
+
+        try {
+            for (int i = 0; ; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    matrixBT[j][i] = matrixB[i][j];
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+
+        try {
+            for (int i = 0; ; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += matrixA[i][k] * matrixBT[j][k];
+                    }
+                    matrixC[i][j] = sum;
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+        return matrixC;
+    }
+
+    // Реализован Шаг 3 из статьи.
+    // Главный недостаток от шага 2 — много циклов. Сокращаем объем кода и делаем его изящнее,
+    // объединив операции транспонирования и умножения в один вычислительный цикл.
+    // При этом, транспонирование будет осуществляться не для целой матрицы (B), а по-столбцам,
+    // по мере их надобности.
+    public static int[][] singleThreadMultiply3(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        final int oneColumnB[] = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                oneColumnB[k] = matrixB[k][j];
+            }
+            for (int i = 0; i < matrixSize; i++) {
+                int oneRowA[] = matrixA[i];
+                int sum = 0;
+                for (int k = 0; k < matrixSize; k++) {
+                    sum += oneRowA[k] * oneColumnB[k];
+                }
+                matrixC[i][j] = sum;
+            }
+        }
+        return matrixC;
+    }
+
+    // Реализован Шаг 4 из статьи на методе singleThreadMultiply3.
+    // Воспользуемся еще одним преимуществом Java — исключениями.
+    // Заменяем проверку на выход за границы матрицы (для внешнего цикла - по столбцам матрицы B) на блок try {} catch {}.
+    // Это сократит количество сравнений на 1000 в нашем случае.
+    // Действительно, зачем 1000 раз сравнивать то, что всегда будет возвращать false и на 1001 раз вернет true.
+    //
+    // С одной стороны — сокращаем количество сравнений,
+    // с другой — появляется дополнительные накладные расходы на обработку исключений.
+    public static int[][] singleThreadMultiply43(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        final int oneColumnB[] = new int[matrixSize];
+        try {
+            for (int j = 0; ; j++) {
+                for (int k = 0; k < matrixSize; k++) {
+                    oneColumnB[k] = matrixB[k][j];
+                }
+                for (int i = 0; i < matrixSize; i++) {
+                    int oneRowA[] = matrixA[i];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += oneRowA[k] * oneColumnB[k];
+                    }
+                    matrixC[i][j] = sum;
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+        return matrixC;
+    }
+
+    // Шаг 5. Была попытка отказаться от проверки окончания в цикле второго уровня.
+    // Но дождаться окончания выполнения этого метода не вышло.
+    public static int[][] singleThreadMultiply543(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        final int oneColumnB[] = new int[matrixSize];
+        try {
+            for (int j = 0; ; j++) {
+                try {
+                    for (int k = 0; ; k++) {
+                        oneColumnB[k] = matrixB[k][j];
+                    }
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+                try {
+                    for (int i = 0; ; i++) {
+                        int oneRowA[] = matrixA[i];
+                        int sum = 0;
+                        for (int k = 0; k < matrixSize; k++) {
+                            sum += oneRowA[k] * oneColumnB[k];
+                        }
+                        matrixC[i][j] = sum;
+                    }
+                }  catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
         }
         return matrixC;
     }
