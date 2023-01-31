b5301f7535ea025726996e4f5694babb775c403c
“Maksim <m.n.timchenko@gmail.com>
2023-01-31 19:11:48 +1000

    Test all five methods multiply of matrixes

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Executors;
 public class MainMatrix {
     private static final int MATRIX_SIZE = 1000;
     private static final int THREAD_NUMBER = 10;
+    private static final int COUNT_OF_PASS = 5;
 
     private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
@@ -16,12 +17,18 @@ public class MainMatrix {
 
         double singleThreadSum = 0.;
         double concurrentThreadSum = 0.;
+        double Sum_concurrentMultiply = 0.;
+        double Sum_concurrentMultiply2 = 0.;
+        double Sum_singleThreadMultiplyOpt2 = 0.;
         int count = 1;
-        while (count < 6) {
+        while (count <= COUNT_OF_PASS) {
             System.out.println("Pass " + count);
-            long start = System.currentTimeMillis();
+            long start;
+            double duration;
+
+            start = System.currentTimeMillis();
             final int[][] matrixC = MatrixUtil.singleThreadMultiplyOpt(matrixA, matrixB);
-            double duration = (System.currentTimeMillis() - start) / 1000.;
+            duration = (System.currentTimeMillis() - start) / 1000.;
             out("Single thread time, sec: %.3f", duration);
             singleThreadSum += duration;
 
@@ -31,15 +38,95 @@ public class MainMatrix {
             out("Concurrent thread time, sec: %.3f", duration);
             concurrentThreadSum += duration;
 
+            // concurrentMultiply
+            start = System.currentTimeMillis();
+            final int[][] MatrixC_concurrentMultiply = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Concurrent thread time, sec: %.3f", duration);
+            Sum_concurrentMultiply += duration;
+
+            // concurrentMultiply2
+            start = System.currentTimeMillis();
+            final int[][] MatrixC_concurrentMultiply2 = MatrixUtil.concurrentMultiply2(matrixA, matrixB, executor);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Concurrent thread time, sec: %.3f", duration);
+            Sum_concurrentMultiply2 += duration;
+
+            // singleThreadMultiplyOpt2
+            start = System.currentTimeMillis();
+            final int[][] MatrixC_singleThreadMultiplyOpt2 = MatrixUtil.singleThreadMultiplyOpt2(matrixA, matrixB);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time, sec: %.3f", duration);
+            Sum_singleThreadMultiplyOpt2 += duration;
+
             if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+                System.err.println("Comparison failed");
+                break;
+            } else if (!MatrixUtil.compare(matrixC, MatrixC_concurrentMultiply)) {
+                System.err.println("Comparison failed");
+                break;
+            } if (!MatrixUtil.compare(matrixC, MatrixC_concurrentMultiply2)) {
+                System.err.println("Comparison failed");
+                break;
+            } if (!MatrixUtil.compare(matrixC, MatrixC_singleThreadMultiplyOpt2)) {
                 System.err.println("Comparison failed");
                 break;
             }
             count++;
         }
+        count--;
         executor.shutdown();
-        out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
-        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+        out("\nAverage single thread time (singleThreadMultiplyOpt), sec: %.3f", singleThreadSum / count);
+        out("Average concurrent thread time (concurrentMultiplyStreams), sec: %.3f", concurrentThreadSum / count);
+
+        out("Average concurrent thread time (concurrentMultiply), sec: %.3f", Sum_concurrentMultiply / count);
+        out("Average concurrent thread time (concurrentMultiply2), sec: %.3f", Sum_concurrentMultiply2 / count);
+        out("Average single thread time (singleThreadMultiplyOpt2), sec: %.3f", Sum_singleThreadMultiplyOpt2 / count);
+
+        /*
+        THREAD_NUMBER = 1
+        Average single thread time (singleThreadMultiplyOpt), sec: 1.236
+        Average concurrent thread time (concurrentMultiplyStreams), sec: 0.100
+        Average concurrent thread time (concurrentMultiply), sec: 0.380
+        Average concurrent thread time (concurrentMultiply2), sec: 0.260
+        Average single thread time (singleThreadMultiplyOpt2), sec: 0.708
+        */
+
+        /*
+        THREAD_NUMBER = 2
+        Average single thread time (singleThreadMultiplyOpt), sec: 1.260
+        Average concurrent thread time (concurrentMultiplyStreams), sec: 0.109
+        Average concurrent thread time (concurrentMultiply), sec: 0.205
+        Average concurrent thread time (concurrentMultiply2), sec: 0.150
+        Average single thread time (singleThreadMultiplyOpt2), sec: 0.716
+        */
+
+        /*
+        THREAD_NUMBER = 4
+        Average single thread time (singleThreadMultiplyOpt), sec: 1.276
+        Average concurrent thread time (concurrentMultiplyStreams), sec: 0.112
+        Average concurrent thread time (concurrentMultiply), sec: 0.119
+        Average concurrent thread time (concurrentMultiply2), sec: 0.101
+        Average single thread time (singleThreadMultiplyOpt2), sec: 0.713
+        */
+
+        /*
+        THREAD_NUMBER = 10
+        Average single thread time (singleThreadMultiplyOpt), sec: 1.250
+        Average concurrent thread time (concurrentMultiplyStreams), sec: 0.113
+        Average concurrent thread time (concurrentMultiply), sec: 0.108
+        Average concurrent thread time (concurrentMultiply2), sec: 0.113
+        Average single thread time (singleThreadMultiplyOpt2), sec: 0.708
+        */
+
+        /*
+        THREAD_NUMBER = 100
+        Average single thread time (singleThreadMultiplyOpt), sec: 1.392
+        Average concurrent thread time (concurrentMultiplyStreams), sec: 0.113
+        Average concurrent thread time (concurrentMultiply), sec: 0.147
+        Average concurrent thread time (concurrentMultiply2), sec: 0.133
+        Average single thread time (singleThreadMultiplyOpt2), sec: 0.682
+        */
     }
 
     private static void out(String format, double ms) {
